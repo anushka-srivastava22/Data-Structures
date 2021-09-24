fdabac8c1cbb622f1d250330c2a7a70aefb57f88
Anushka Srivastava <67271376+anushka-srivastava22@users.noreply.github.com>
2021-09-24 12:56:14 +0530

    Create MinMovesClimbStairs.java

---
+++ Dynamic Programming/MinMovesClimbStairs.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+      
+        Integer dp[] = new Integer[n+1];
+        dp[n] = 0;
+        for(int i=n-1;i>=0;i--)
+        {
+            int min = Integer.MAX_VALUE;
+            for(int j=1;j<=arr[i] && i+j < dp.length;j++)
+            {
+                if(dp[i+j] != null)
+                {
+                    min = Math.min(min,dp[i+j]);
+                }
+            }
+            if(min != Integer.MAX_VALUE)
+            {
+                dp[i] = min + 1;
+            }
+        }
+        System.out.println(dp[0]);
+    }
+    
+
+}
